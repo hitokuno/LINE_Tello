f08059e6db77c6e06f5a6f2e0536caa7af61e2b9
hitokuno <hitokuno@aol.com>
2018-10-10 22:37:39 +0900

    Add reply api

---
+++ api/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+
+def index(request):
+    return HttpResponse("This is bot api.")
+
+def callback(request):
+    reply = ""
+    request_json = json.loads(request.body.decode('utf-8')) # requestの情報をdict形式で取得
+    for e in request_json['events']:
+        reply_token = e['replyToken']  # 返信先トークンの取得
+        message_type = e['message']['type']   # typeの取得
+
+        if message_type == 'text':
+            text = e['message']['text']    # 受信メッセージの取得
+    return HttpResponse(text)  # テスト用
